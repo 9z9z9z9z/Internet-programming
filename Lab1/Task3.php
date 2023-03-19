df04259b00bd196d83e4d44f5e27bd8bdf24089b
9z9z9z9z <72688414+9z9z9z9z@users.noreply.github.com>
2023-03-19 19:05:27 +0300

    Add files via upload

---
+++ Lab1/Task3.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Пробная страница </title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="save3.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="first_col col-md-4 col-12 order-sm-1 order-3">Первая колонка длиной 4</div>
+                <div class="second_col col-md-2 col-12 order-sm-2 order-2">Вторая колонка длиной 2</div>
+                <div class="third_col col-md-6 col-12 order-sm-3 order-1">Третья колонка длиной 6</div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ Lab1/Task4.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Пробная страница </title>
+        <link rel="stylesheet" href="save4.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="column-1">Первая колонка</div>
+                <div class="column-2">Вторая колонка</div>
+                <div class="column-3">Третья колонка</div>
+            </div>
+        </div>
+    </body>
+</html>
