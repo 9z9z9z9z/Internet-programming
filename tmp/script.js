fb07a75773e537a22ff6f2ed30cb30453904b3ea
9z9z9z9z <72688414+9z9z9z9z@users.noreply.github.com>
2023-04-03 16:51:06 +0300

    Add files via upload

--- tmp/script.js
+++ tmp/script.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
         }
     });
 
-    $(document).on("Change keyup input click", "input[type='num']", function() {
+    $(document).on("Change keyup input click", "input[type='text']", function() {
         if(this.value.match(/[^0-9]/g)) {
             this.value = this.value.replace(/[^0-9]/g, "");
         };
